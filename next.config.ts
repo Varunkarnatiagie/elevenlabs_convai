a1b973e4d0ce7dcda38bf9d81010e5b8ffdf502e
Varunkarnatiagie <varun.karnati@agie.ai>
2024-12-12 18:33:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enables static export
-  basePath: "elevenlabs_convai", // Replace with your GitHub repository name
-  assetPrefix: "/your-repo-name", // Prefix for assets, required for GitHub Pages
+  basePath: "/elevenlabs_convai", // Replace with your GitHub repository name
+  assetPrefix: "/elevenlabs_convai", // Prefix for assets, required for GitHub Pages
 };
 
 export default nextConfig;
