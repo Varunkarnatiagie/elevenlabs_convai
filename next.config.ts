dfd06276cd013a158884f3f81c128e0c032c2b15
varunkarnati <88199687+varunkarnati@users.noreply.github.com>
2024-12-12 23:49:19 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export", // Enables static export
-  basePath: "/elevenlabs_convai", // Replace with your GitHub repository name
-  assetPrefix: "/elevenlabs_convai", // Prefix for assets, required for GitHub Pages
+  output: "export", // Ensures static export is enabled
 };
 
 export default nextConfig;
